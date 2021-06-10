3e0cbca0ff2721362f392cd2ca2b9f7d214fb9a9
benjamin maureau <benjamin.maureau@hotmail.fr>
2021-06-10 17:14:37 +0200

    list movies

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,18 @@ require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap';
 
+import { loadDynamicBannerText } from '../components/_banner';
+import { initSelect2 } from '../components/_init_select2';
+import { initUpdateNavbarOnScroll } from '../components/_navbar';
+
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  // [...]
+  initUpdateNavbarOnScroll();
+  loadDynamicBannerText();
+  initSelect2();
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
